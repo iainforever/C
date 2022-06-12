3480bdd14c64b9c7dec52317bff41c9722ecd989
iainforever <104896823+iainforever@users.noreply.github.com>
2022-06-12 18:29:51 +0800

    Create cpractice2.12.6.c

---
+++ cpractice2.12.6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (void)
+{
+	int toes = 10;
+	printf("toes = %d\n",toes);
+	printf("2 * toes = %d\n",2 * toes);
+	printf("toes * toes = %d\n",toes * toes);
+	
+	return 0; 
+}
