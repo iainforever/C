f0e5c3bd18b550e6fe4250ba218b63d4d45ad0ee
iainforever <104896823+iainforever@users.noreply.github.com>
2022-07-11 16:21:25 +0800

    Create c2.5.c

---
+++ c2.5.c
@@ -0,0 +1,13 @@
+/*修复了语法错误的程序*/
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    int n, n2, n3;
+    n = 5;
+    n2 = n * n;
+    n3 = n2 * n2;
+    printf("n = %d, n squared = %d, n cubed = %d", n, n2, n3);
+    
+    return 0;
+}
